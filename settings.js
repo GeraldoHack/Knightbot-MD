d5179c8a47599b5c0d01f124f86bb9a752b6b467
GeraldoHack <geraldoafonso042@gmail.com>
2025-02-24 10:43:10 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Geraldo-Bot",
+  botOwner: 'Geraldo', // Your name
+  ownerNumber: '258878877665', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
